5b9f18b08b7c7d0641ee46486f7ff0ce29ccf5f4
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2023-10-30 22:21:12 +0100

    Initial Commit

---
+++ NoEscape/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  NoEscape
+//
+//  Created by Luís Carvalho on 30.10.2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
